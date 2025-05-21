d803649ab18a6e0c2c9a80a993d75e8a8d449825
Shree Hari N G <shreeharing20@gmail.com>
2025-05-21 17:46:09 +0530

    Completed day 9 and 10 projects

---
+++ Day10_music_player/script.js
@@ -0,0 +1,34 @@
+let progress = document.getElementById("progress")
+let song = document.getElementById("song")
+let ctrlIcon = document.getElementById("cltrIcon")
+
+song.onloadedmetadata = function(){
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+}
+
+function playPause(){
+    console.log("hii");
+    if(ctrlIcon.classList.contains("fa-pause")){
+        song.pause();
+        ctrlIcon.classList.remove("fa-pause"); 
+        ctrlIcon.classList.add("fa-play");
+    }
+    else{
+        song.play();
+        ctrlIcon.classList.remove("fa-play"); 
+        ctrlIcon.classList.add("fa-pause");
+    }
+}
+
+if(song.play()){
+    setInterval(()=>{
+        progress.value = song.currentTime;
+    },500)
+}
+progress.onchange = function(){
+    song.play();
+    song.currentTime = progress.value;
+    ctrlIcon.classList.add("fa-pause");
+    ctrlIcon.classList.remove("fa-play");
+}
